fbe86d414aa8bd7676c0d662f40b910f4c7c9356
sorochinskkydm <sorochinskkydm@gmail.com>
2024-01-08 23:58:56 +0300

    added files and users resources & database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { FilesModule } from "./files/files.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    FilesModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "BrosGriefy1998",
+      database: "storage",
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
